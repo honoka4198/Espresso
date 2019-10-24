77ed8d6961eb9a396f324d5bd9d1bfffd368e72a
Akisu <2180418@s2180418.maetel.ecc.ac.jp>
2019-10-24 16:41:52 +0900

    Gitにファイル登録

---
+++ src/views/components/header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../../styles/components/header.css"
+
+class Header extends React.Component {
+  render() {
+    return (
+        <div>
+          <div className="header">
+            <h1>Header</h1>
+          </div>
+        </div>
+    )
+  }
+}
+
+export default Header;
