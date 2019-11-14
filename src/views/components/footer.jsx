be0f64dab4dee853320db5d21b91cee5d6d9f7cb
nishio yuka <arupaka151@yahoo.co.jp>
2019-11-14 19:22:51 +0900

    footerファイルの作成・変更

--- src/views/components/footer.jsx
+++ src/views/components/footer.jsx
@@ -1,21 +1,25 @@
 import React from 'react'
 import '../../styles/components/footer.css';
+import { BrowserRouter,Link } from 'react-router-dom';
 
 class Footer extends React.Component {
   render() {
     return (
-      <div className="footer">
-        <ul id="footerNavi">
-            <li><link to="/question">よくあるご質問  | </link></li> 
-            <li><link to="/payment_methods">お支払い方法  | </link></li>
-            <li><link to="/delivery_charge">配送料について  | </link></li>
-            <li><link to="/TOS">ご利用規約  | </link></li>
-            <li><link to="/notation">特定商取引法に基づく表記  | </link></li>
-            <li><link to="/policy">プライバシー＆セキュリティーポリシー</link></li>
-        </ul>
-        <p id="Copyright">Copyright @ 2019 coffee shop.</p>
-      </div>
+      <BrowserRouter>
+        <div className="footer">
+          <ul id="footerNavi">
+            <li><Link to="question">よくあるご質問</Link></li><li>  | </li>
+            <li><Link to="payment_methods">お支払い方法</Link></li><li>  | </li>
+            <li><Link to="delivery_charge">配送料について</Link></li><li>  | </li>
+            <li><Link to="TOS">ご利用規約</Link></li><li>  | </li>
+            <li><Link to="notation">特定商取引法に基づく表記</Link></li><li>  | </li>
+            <li><Link to="policy">プライバシー＆セキュリティーポリシー</Link></li>
+          </ul>
+          <p id="Copyright">Copyright @ 2019 coffee shop.</p>
+        </div>
+      </BrowserRouter>
     );
+    
   }
 }
 
