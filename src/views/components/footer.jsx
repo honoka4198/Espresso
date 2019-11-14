468b8024e3fbb505910183997e2e7e43bbec6eee
nishio yuka <arupaka151@yahoo.co.jp>
2019-11-14 18:50:47 +0900

    footerファイルの作成・変更

--- src/views/components/footer.jsx
+++ src/views/components/footer.jsx
@@ -5,7 +5,15 @@ class Footer extends React.Component {
   render() {
     return (
       <div className="footer">
-        <h1>Footer</h1>
+        <ul id="footerNavi">
+            <li><link to="/question">よくあるご質問  | </link></li> 
+            <li><link to="/payment_methods">お支払い方法  | </link></li>
+            <li><link to="/delivery_charge">配送料について  | </link></li>
+            <li><link to="/TOS">ご利用規約  | </link></li>
+            <li><link to="/notation">特定商取引法に基づく表記  | </link></li>
+            <li><link to="/policy">プライバシー＆セキュリティーポリシー</link></li>
+        </ul>
+        <p id="Copyright">Copyright @ 2019 coffee shop.</p>
       </div>
     );
   }
