9dc30eff3ccc78d626fa8f6bcc59898008e2c20a
nishio yuka <arupaka151@yahoo.co.jp>
2019-11-18 11:22:16 +0900

    .json変更

--- src/views/components/footer.jsx
+++ src/views/components/footer.jsx
@@ -8,11 +8,11 @@ class Footer extends React.Component {
       <BrowserRouter>
         <div className="footer">
           <ul id="footerNavi">
-            <li><Link to="question">よくあるご質問</Link></li><li>  | </li>
-            <li><Link to="payment_methods">お支払い方法</Link></li><li>  | </li>
-            <li><Link to="delivery_charge">配送料について</Link></li><li>  | </li>
-            <li><Link to="TOS">ご利用規約</Link></li><li>  | </li>
-            <li><Link to="notation">特定商取引法に基づく表記</Link></li><li>  | </li>
+            <li><Link to="question">よくあるご質問</Link></li><li>|</li>
+            <li><Link to="payment_methods">お支払い方法</Link></li><li>|</li>
+            <li><Link to="delivery_charge">配送料について</Link></li><li>|</li>
+            <li><Link to="TOS">ご利用規約</Link></li><li>|</li>
+            <li><Link to="notation">特定商取引法に基づく表記</Link></li><li>|</li>
             <li><Link to="policy">プライバシー＆セキュリティーポリシー</Link></li>
           </ul>
           <p id="Copyright">Copyright @ 2019 coffee shop.</p>
