da113af9fb6cf0fb5ba9dcda18d37d8bc7756043
akisu1016 <40588884+akisu1016@users.noreply.github.com>
2019-11-21 15:53:31 +0900

    Revert "Develop"

--- src/views/components/footer.jsx
+++ src/views/components/footer.jsx
@@ -1,25 +1,13 @@
 import React from 'react'
 import '../../styles/components/footer.css';
-import { BrowserRouter,Link } from 'react-router-dom';
 
 class Footer extends React.Component {
   render() {
     return (
-      <BrowserRouter>
-        <div className="footer">
-          <ul id="footerNavi">
-            <li><Link to="question">よくあるご質問</Link></li><li>|</li>
-            <li><Link to="payment_methods">お支払い方法</Link></li><li>|</li>
-            <li><Link to="delivery_charge">配送料について</Link></li><li>|</li>
-            <li><Link to="TOS">ご利用規約</Link></li><li>|</li>
-            <li><Link to="notation">特定商取引法に基づく表記</Link></li><li>|</li>
-            <li><Link to="policy">プライバシー＆セキュリティーポリシー</Link></li>
-          </ul>
-          <p id="Copyright">Copyright @ 2019 coffee shop.</p>
-        </div>
-      </BrowserRouter>
+      <div className="footer">
+        <h1>Footer</h1>
+      </div>
     );
-    
   }
 }
 
