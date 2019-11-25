06071c4da416a99ea9c4485e3a9fd77ad228254e
杉森 史英 <2180316@MAETEL.ECC.AC.JP>
2019-11-25 12:26:38 +0900

    新規登録フォーム

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/index.css';
-import LoginFormApp from './views/LoginFormApp';
 import App from './views/App';
 import * as serviceWorker from './views/serviceWorker';
 
