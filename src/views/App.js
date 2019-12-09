d8e846ed04ff8698643b8c55facd6113591b8ae3
akisu1016 <40588884+akisu1016@users.noreply.github.com>
2019-12-09 12:26:30 +0900

    Update App.js

--- src/views/App.js
+++ src/views/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Header from './components/header';
 import Footer from './components/footer';
 import Sidelist from './components/sidelist';
-import Contact_form from './components/contact_form';
 
 
 class App extends Component {
